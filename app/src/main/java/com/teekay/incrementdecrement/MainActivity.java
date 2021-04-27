2e3976d1a73bb89a174eb6c74cdd2813adc05e9e
prinxpraiz <80994381+prinxpraiz@users.noreply.github.com>
2021-04-27 16:35:24 +0100

    Update MainActivity.java

--- app/src/main/java/com/teekay/incrementdecrement/MainActivity.java
+++ app/src/main/java/com/teekay/incrementdecrement/MainActivity.java
@@ -3,6 +3,9 @@ package com.teekay.incrementdecrement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,20 +18,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        increase = findViewById(R.id.increment); decrease = findViewById(R.id.decrement);
+        increase = findViewById(R.id.increment); decrease = findViewById (R.id.decrement);
         textV = findViewById(R.id.textView);
 
         increase.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 count++;
-                textV.setText(count);
+                textV.setText(count +" Clicks!");
             }
         });
         decrease.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 count--;
-                textV.setText(count);
+                textV.setText(count + " Clicks!");
+            }
+        });
     }
 }
